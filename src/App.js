890e64c689da662f33c5b7991cad95d50d987171
Nick Carducci <nmcarducci@gmail.com>
2021-03-17 18:26:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
     "STOP ASKING FOR CAPACITY WHEN YOU MEAN DEMAND CAPACITY WHICH IS PERFECTLY PRICE ELASTIC FOR ITS UTILITY PER EACH NEW CAPACITY, WE NEED  SUPPLY CAPACITY OR VOLUNTEER OR LESS FRUGAL PRODUCERS, NOT STEAL BY COUNTERFEIT-IN-CONTRACT";
   return (
     <div className="App">
+      Conspiracy theories are detective work
+      <br />
+      <br />
       "March 17th, 2021
       <br />
       $1400 + $1400 dependent
