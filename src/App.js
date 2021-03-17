f3fd11595efa258d410d6fc19afa421a4941df3f
Nick <nmcarducci@gmail.com>
2021-03-17 14:47:09 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        "Show me what you need, I'll get those contractors down there" - Joe
+        Biden
+      </div>
+      <a href="https://www.researchgate.net/project/Class-action-suit">
+        Class action suit
+      </a>
+    </div>
+  );
+}
