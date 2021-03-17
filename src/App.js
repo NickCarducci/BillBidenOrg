6b1269ac710a60fcc89d508584320d2ea12c9074
Nick Carducci <nmcarducci@gmail.com>
2021-03-17 14:58:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ export default function App() {
   return (
     <div className="App">
       <div>
-        revolution is self-defense while&nbsp;
+        <a href="https://www.academia.edu/43983364/Review_Capitalism_in_the_21st_Century">
+          revolution
+        </a>
+        &nbsp;is self-defense while&nbsp;
         <a href="https://vaults.biz">vault.biz</a>
         &nbsp;exists
       </div>
@@ -13,7 +16,8 @@ export default function App() {
       <div>
         "Show me what you need, I'll get those contractors down there" - Joe
         Biden. I bought this domain, for a year, with the partial
-        equityutilitymoney split Joe and Donald tweaked out
+        equityutilitymoney split Joe and Donald tweaked out of my
+        equityutilitymoney
       </div>
       <a href="https://www.researchgate.net/project/Class-action-suit">
         Class action suit
