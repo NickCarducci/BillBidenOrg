5bdefae6a44a1fbe557d0e308017da2a6ee929e9
Nick Carducci <nmcarducci@gmail.com>
2021-03-19 18:00:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ export default function App() {
           position: "relative"
         }}
       >
+        Nicole Maliotakis wants to launder the equityutilitymoney split to not pay down debt but delay sales tax but still pay out to bonds and your contractor-friends.  Total monopsony and playcation for ulterior motive of money. boo hoo nicole m
+        <br/>
+        <br/>
         "All the billions to the states can certainly wipe out the DEFICIT that
         exists"&nbsp;
         <a href="https://en.wikipedia.org/wiki/David_Paterson">Paterson</a>,
